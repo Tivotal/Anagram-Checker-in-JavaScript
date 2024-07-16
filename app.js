004c6693f47a556fea5a198a26a394b0427b0e4f
Tivotal <121335284+Tivotal@users.noreply.github.com>
2024-07-16 08:31:56 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+/* Created by Tivotal */
+
+let input1 = document.querySelector(".input1");
+let input2 = document.querySelector(".input2");
+let btn = document.querySelector(".btn");
+let result = document.querySelector(".result");
+
+btn.addEventListener("click", () => {
+  //getting input values provided by user
+  let word1 = input1.value.toLowerCase();
+  let word2 = input2.value.toLowerCase();
+
+  //removing spaces and punctuation from both words
+  word1 = word1.replace(/[^\w]/g, "");
+  word2 = word2.replace(/[^\w]/g, "");
+
+  //comparing the lengths of both words
+  if (word1.length !== word2.length) {
+    result.textContent = "Words are not Anagrams";
+    result.classList.add("negative");
+    result.classList.remove("positive");
+  }
+
+  //letter occurences count in both words
+  let count1 = {};
+  let count2 = {};
+
+  for (let char of word1) {
+    count1[char] = (count1[char] || 0) + 1;
+  }
+
+  for (let char of word2) {
+    count2[char] = (count2[char] || 0) + 1;
+  }
+
+  //comparing letter count of both the words
+  for (let char in count1) {
+    if (count1[char] !== count2[char]) {
+      result.textContent = "Words are not Anagrams";
+      result.classList.add("negative");
+      result.classList.remove("positive");
+      return;
+    }
+  }
+
+  result.textContent = "Words are Anagrams";
+  result.classList.remove("negative");
+  result.classList.add("positive");
+});
